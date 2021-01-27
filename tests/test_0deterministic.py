1bfdbc9ed022d82bd865813cf54a617a5662c0b2
Antonin RAFFIN <antonin.raffin@ensta.org>
2021-01-27 15:22:58 +0100

    Separate TD3 test

--- tests/test_0deterministic.py
+++ tests/test_0deterministic.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 from stable_baselines import A2C, ACER, ACKTR, DQN, DDPG, PPO1, PPO2, SAC, TRPO, TD3
 from stable_baselines.common.noise import NormalActionNoise
@@ -9,7 +10,28 @@ SEED = 0
 
 # Weird stuff: TD3 would fail if another algorithm is tested before
 # with n_cpu_tf_sess > 1
-@pytest.mark.parametrize("algo", [A2C, ACKTR, ACER, DDPG, DQN, PPO1, PPO2, SAC, TRPO, TD3])
+def test_deterministic_td3():
+    results = [[], []]
+    rewards = [[], []]
+    kwargs = {'n_cpu_tf_sess': 1}
+    env_id = 'Pendulum-v0'
+    kwargs.update({'action_noise': NormalActionNoise(0.0, 0.1)})
+
+    for i in range(2):
+        model = TD3('MlpPolicy', env_id, seed=SEED, **kwargs)
+        model.learn(N_STEPS_TRAINING)
+        env = model.get_env()
+        obs = env.reset()
+        for _ in range(20):
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, _, _ = env.step(action)
+            results[i].append(action)
+            rewards[i].append(reward)
+    assert sum(results[0]) == sum(results[1]), results
+    assert sum(rewards[0]) == sum(rewards[1]), rewards
+
+
+@pytest.mark.parametrize("algo", [A2C, ACKTR, ACER, DDPG, DQN, PPO1, PPO2, SAC, TRPO])
 def test_deterministic_training_common(algo):
     results = [[], []]
     rewards = [[], []]
